e665d1f845cc8e0d9e48dc030f3d08b98dc9b062
Michael Pheasant <mpheasant@gmail.com>
2017-03-23 16:24:01 +1000

    move Turducken up to components and use destructuring in Box

--- docs/tinymce-single-react-ui/src/components/box/Box.js
+++ docs/tinymce-single-react-ui/src/components/box/Box.js
@@ -2,15 +2,13 @@ import React, { createElement, Component } from 'react';
 import ReactDOM from 'react-dom';
 import styles from './box.scss';
 
-export default function Box(props) {
-  let r = props.rect
-
-	return (r && r.width) ? (
+export default function Box({rect}) {
+	return rect ? (
     <div>
-      <div className={styles.box} style={ {background: '#ccc', position: 'absolute', top: r.top + 'px', width: r.width + 'px', height: '2px', } }></div>
-      <div className={styles.box} style={ {background: '#ccc', position: 'absolute', top: r.height + r.top + 'px', width: r.width + 'px', height: '2px' } }></div>
-      <div className={styles.box} style={ {background: '#ccc', position: 'absolute', top: r.top + 'px', width: '2px', height: r.height + 'px' } }></div>
-      <div className={styles.box} style={ {background: '#ccc', position: 'absolute', top: r.top + 'px', width: '2px', height: r.height + 'px', left: r.width + r.left + 'px' } }></div>
+      <div className={styles.box} style={ {background: '#ccc', position: 'absolute', top: rect.top + 'px', width: rect.width + 'px', height: '2px', } }></div>
+      <div className={styles.box} style={ {background: '#ccc', position: 'absolute', top: rect.height + rect.top + 'px', width: rect.width + 'px', height: '2px' } }></div>
+      <div className={styles.box} style={ {background: '#ccc', position: 'absolute', top: rect.top + 'px', width: '2px', height: rect.height + 'px' } }></div>
+      <div className={styles.box} style={ {background: '#ccc', position: 'absolute', top: rect.top + 'px', width: '2px', height: rect.height + 'px', left: rect.width + rect.left + 'px' } }></div>
     </div>
 	)
   : null
--- docs/tinymce-single-react-ui/src/index.js
+++ docs/tinymce-single-react-ui/src/index.js
@@ -2,7 +2,7 @@ import React, { createElement, Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-import Turducken from './Turducken';
+import Turducken from './containers/Turducken';
 import content from './reducers/content'
 
 const store = createStore(content)
@@ -18,3 +18,36 @@ render()
 store.subscribe(render)
 
 // TODO: wrap Turducken in a Provider and add the react-redux stuff
+/*
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+import { browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { AppContainer } from 'react-hot-loader';
+import configureStore from './store/configureStore';
+import Root from './containers/Root';
+
+const store = configureStore();
+const history = syncHistoryWithStore(browserHistory, store);
+
+render(
+    <AppContainer>
+        <Root store={store} history={history} />
+    </AppContainer>,
+    document.getElementById('root')
+);
+
+if (module.hot) {
+    module.hot.accept('./containers/Root', () => {
+        const NewRoot = require('./containers/Root').default;
+        render(
+            <AppContainer>
+                <NewRoot store={store} history={history} />
+            </AppContainer>,
+            document.getElementById('root')
+        );
+    });
+}
+
+*/
